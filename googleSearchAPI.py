3c2cbbc6b26250e09728c559c0733b1f216d1387
kumarhit <hitesh.kumar1@merck.com>
2017-04-17 20:14:59 -0400

    latest code

---
+++ googleSearchAPI.py
@@ -0,0 +1,20 @@
+import pprint
+
+from googleapiclient.discovery import build
+
+
+def main():
+  # Build a service object for interacting with the API. Visit
+  # the Google APIs Console <http://code.google.com/apis/console>
+  # to get an API key for your own application.
+  service = build("customsearch", "v1",
+            developerKey="AIzaSyAyO9ZSUrVBFbgPhE7_iaXDBRr35F_SdwQ")
+
+  res = service.cse().list(
+      q='lectures',
+      cx='001651119929652751903:hsy37cecsye',
+    ).execute()
+  pprint.pprint(res)
+
+if __name__ == '__main__':
+  main()
