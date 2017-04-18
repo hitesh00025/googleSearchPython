a9301cc527d750181c69f22eb872c2f94a802275
Hitesh Kumar <hk583366@gmail.com>
2017-04-17 20:45:28 -0400

    Update googleSearchAPI.py

--- googleSearchAPI.py
+++ googleSearchAPI.py
@@ -7,6 +7,8 @@ def main():
   # Build a service object for interacting with the API. Visit
   # the Google APIs Console <http://code.google.com/apis/console>
   # to get an API key for your own application.
+  # NOTE : use your own KEY and SEARCH KEY
+  
   service = build("customsearch", "v1",
             developerKey="AIzaSyAyO9ZSUrVBFbgPhE7_iaXDBRr35F_SdwQ")
 
